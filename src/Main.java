564fdbc03ddfcc3da97e7d1dfa390b3b41a76fbc
Lenasenap <lena.stridsberg@teliacompany.com>
2022-09-16 09:44:52 +0200

    Add class Animal and change main to create a few objects

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Dag 8/20!");
+        String sayHello = "Hej och godmorgon!";
+        System.out.println(sayHello);
+
+        Animal horse1 = new Animal();
+        Animal horse2 = new Animal(3);
+        Animal horse3 = new Animal(3, "Åke", "Häst");
+        Animal horse4 = new Animal("Häst");
+
+        System.out.println(horse1.getAge());
+        System.out.println(horse2.getAge());
+        System.out.println(horse3.getType());
+        System.out.println(horse4.getType());
     }
 }
